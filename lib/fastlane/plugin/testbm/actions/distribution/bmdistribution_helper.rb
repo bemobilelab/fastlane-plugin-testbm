b874ee8f8d9c816af47fb15a9fa65b0d7ee2ccb0
Ramses Velasquez <ramses@bemobile.es>
2021-11-10 11:22:46 +0100

    firebase debug

--- lib/fastlane/plugin/testbm/actions/distribution/bmdistribution_helper.rb
+++ lib/fastlane/plugin/testbm/actions/distribution/bmdistribution_helper.rb
@@ -44,13 +44,13 @@ module Fastlane
     
         firebase_login_token = ENV["FIREBASE_LOGIN_TOKEN"]
         if fabric_groups.length > 0 && fabric_mails.length > 0
-            other_action.firebase_app_distribution(app: firebase_app_id, firebase_cli_token: firebase_login_token, testers: fabric_mails, groups: fabric_groups, release_notes: fabric_notes)
+            other_action.firebase_app_distribution(app: firebase_app_id, firebase_cli_token: firebase_login_token, testers: fabric_mails, groups: fabric_groups, release_notes: fabric_notes, debug: true)
         elsif fabric_groups.length > 0
-            other_action.firebase_app_distribution(app: firebase_app_id, firebase_cli_token: firebase_login_token, groups: fabric_groups, release_notes: fabric_notes)
+            other_action.firebase_app_distribution(app: firebase_app_id, firebase_cli_token: firebase_login_token, groups: fabric_groups, release_notes: fabric_notes, debug: true)
         elsif fabric_mails.length > 0
-            other_action.firebase_app_distribution(app: firebase_app_id, firebase_cli_token: firebase_login_token, testers: fabric_mails, release_notes: fabric_notes)
+            other_action.firebase_app_distribution(app: firebase_app_id, firebase_cli_token: firebase_login_token, testers: fabric_mails, release_notes: fabric_notes, debug: true)
         else
-            other_action.firebase_app_distribution(app: firebase_app_id, firebase_cli_token: firebase_login_token, release_notes: fabric_notes)
+            other_action.firebase_app_distribution(app: firebase_app_id, firebase_cli_token: firebase_login_token, release_notes: fabric_notes, debug: true)
         end
     
         other_action.bmslacknewversion(destiny: "Firebase", app_information: app_information, platform_type: platform_type)
