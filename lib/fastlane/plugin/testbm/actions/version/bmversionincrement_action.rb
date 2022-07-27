5ce3907244b44c10888834616742eaf34f307f5b
oriol-pastor <105732999+oriol-pastor@users.noreply.github.com>
2022-07-27 17:36:44 +0200

    upload android version name and number automaticaly

--- lib/fastlane/plugin/testbm/actions/version/bmversionincrement_action.rb
+++ lib/fastlane/plugin/testbm/actions/version/bmversionincrement_action.rb
@@ -4,13 +4,13 @@ module Fastlane
   module Actions
     class BmversionincrementAction < Action
       def self.run(params)
-        app_information = params[:app_information]
+        project_information = params[:project_information]
         platform_type = params[:platform_type]
 
         if platform_type == Helper::BmHelper::CONST_PROJECT_TYPE__IOS 
             puts "PENDING CODE"
         elsif
-            Helper::BmVersion::increment_build_number_for_android(other_action, app_information)
+            Helper::BmVersion::increment_build_number_for_android(other_action, project_information)
         end
       end
 
@@ -32,11 +32,11 @@ module Fastlane
 
       def self.available_options
         [
-            FastlaneCore::ConfigItem.new(key: :app_information,
+            FastlaneCore::ConfigItem.new(key: :project_information,
                             env_name: "APP_INFORMATION",
                         description: "The app information including name, version",
                             optional: false,
-                                type: Hash),
+                                type: BmProjectInformation),
             FastlaneCore::ConfigItem.new(key: :platform_type,
                             env_name: "PLATFORM_TYPE",
                         description: "Indicates platform wheter android or ios",
