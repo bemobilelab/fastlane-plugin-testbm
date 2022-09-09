fc11b8abb9a1b35b0d72a76ec1fdeaaf9f287ed8
Lluís Morató Guardia <lmorato@bemobile.es>
2022-09-09 12:24:39 +0200

    WIP! Version and bundle management by reading and writing

--- lib/fastlane/plugin/testbm/actions/version/bmversion_helper.rb
+++ lib/fastlane/plugin/testbm/actions/version/bmversion_helper.rb
@@ -1,14 +1,13 @@
 module Fastlane
     module Helper
         class BmVersion
-
-            def self.get_version(platform_type)
+            def self.get_version(project_information:, platform_type:)
                 build_number = ""
                 version_number = ""
             
                 if platform_type == Helper::BmHelper::CONST_PROJECT_TYPE__IOS
-                    version_number = Actions.lane_context[Actions::SharedValues::VERSION_NUMBER]
-                    build_number = Actions.lane_context[Actions::SharedValues::BUILD_NUMBER]
+                    version_number = get_info_plist_value(project_information: project_information, key: "CFBundleShortVersionString")
+                    build_number = get_info_plist_value(project_information: project_information, key: "CFBundleVersion")
                 elsif
                     version_number = File.read("version.name").to_s  
                     build_number = File.read("version.number").to_s  
@@ -17,40 +16,42 @@ module Fastlane
                 {build_number: build_number, version_number: version_number}
             end
 
-
-
             #Increment the build number and the version number, then makes a commit and push to git with a tag
             #The build number is set with the date and hour 
             #The version number is incremented in the patch part by 0.0.1
             #The push is made to the current working branch
-
-
-            def self.increment_build_number_for_android(other_action, app_information)
-                self.version_func_update_build_number(other_action: other_action, app_information: app_information, platform_type: Helper::BmHelper::CONST_PROJECT_TYPE__ANDROID)
-            end
-
-            def version_func_update_build_number_for_ios(app_information:)
-                version_func_update_build_number(app_information: app_information, platform_type: CONST_PROJECT_TYPE__IOS)
-            end
 
             def version_func_update_build_number_for_hibrid(app_information:)
                 #TODO, pendiente de hacer
             end
 
             # Aqui hay que ver si vale la pena solo pasra el tipo de plataforma o otda la info. 
-            def self.version_func_update_build_number(other_action:, app_information:, platform_type:)
+            def self.version_func_update_build_number(other_action:, project_information:, platform_type:)
 
                 #check that the git is clean so the only change that is commited is the new version number
                 other_action.ensure_git_status_clean()
                 
-                #build_number = version_func_get_time_with_build_number_format() 
+                build_number = version_func_get_time_with_build_number_format() 
                 
                 if platform_type == Helper::BmHelper::CONST_PROJECT_TYPE__IOS
+                    #versionNumber = get_info_plist_value(project_information: project_information, key: "CFBundleShortVersionString")
+                    #buildNumber = get_info_plist_value(project_information: project_information, key: "CFBundleVersion")
+
+                    #puts "######################## versionNumber: #{versionNumber} ############"
+                    #puts "######################## buildNumber: #{buildNumber} ############"
+
+
+
+
+
+                    #puts "######################## build_number: #{build_number} ############"
                     # TODO Falta hacer el build_numebr com en adnroid 
 
                     #this functions increment/set the version in all the schemas of the project
-                    increment_build_number(build_number: build_number, xcodeproj: app_information[:ios][:xcodeproj])
-                    increment_version_number(bump_type: "patch")
+                    set_info_plist_value(project_information: project_information, key: "CFBundleVersion", value: build_number)
+                    #set_info_plist_value(project_information:, ke
+                    #increment_build_number(build_number: build_number)
+                    #increment_version_number(bump_type: "patch")
 
                 elsif 
 
@@ -69,14 +70,12 @@ module Fastlane
                     File.write("version.number", build_number)
                 end
 
-                version_info = self.get_version(platform_type:platform_type)
+                version_info = self.get_version(project_information: project_information, platform_type: platform_type)
                 final_version_number = version_info[:version_number]
                 final_build_number = version_info[:build_number]
 
-
                 message = "Build from fastlane #{final_build_number} #{final_version_number}"
                 complet_git_message = "#{message}. [skip ci]"
-
 
                 if platform_type == Helper::BmHelper::CONST_PROJECT_TYPE__IOS
                     commit_version_bump(message: complet_git_message) #[skip ci] makes that de ci ignores this push
@@ -92,7 +91,48 @@ module Fastlane
                 other_action.push_to_git_remote(local_branch: "HEAD", remote_branch: actual_branch_name)
                 
             end
-  
+
+            def self.version_func_get_time_with_build_number_format()
+                time = Time.now.utc
+                format_for_time = "%d%02d%02d%02d%02d"
+                time_parts_to_format = [ time.year, time.month, time.day, time.hour, time.min ]
+                time_with_format = format_for_time % time_parts_to_format
+                return time_with_format[2..11]
+            end
+
+            def self.get_info_plist_value(project_information:, key:)
+                begin
+                    infoPlistPath = "./#{ENV['PRIVATE_APP_NAME']}/#{project_information.get_ios_info_plist_name}.plist"
+                    path = File.expand_path(infoPlistPath)
+            
+                    plist = File.open(path) { |f| Plist.parse_xml(f) }
+            
+                    value = plist[key]
+            
+                    return value
+                rescue => ex
+                    UI.error(ex)
+                end
+            end
+
+            def self.set_info_plist_value(project_information:, key:, value:)
+                begin
+                    infoPlistPath = "./#{ENV['PRIVATE_APP_NAME']}/#{project_information.get_ios_info_plist_name}.plist"
+                    path = File.expand_path(infoPlistPath)
+                    plist = Plist.parse_xml(path)
+
+                    plist[key] = value
+                    
+                    new_plist = Plist::Emit.dump(plist)
+                    
+                    File.write(path, new_plist)
+        
+                    return value
+                rescue => ex
+                    UI.error(ex)
+                    UI.user_error!("Unable to set value to plist file at '#{path}'")
+                end
+            end
         end
     end
   end
---
+++ lib/fastlane/plugin/testbm/actions/version/bmversionincrementios_action.rb
@@ -0,0 +1,43 @@
+require 'fastlane/action'
+
+module Fastlane
+    module Actions
+        class BmversionincrementiosAction < Action
+            def self.run(params)
+                project_information = params[:project_information]
+                Helper::BmVersion::version_func_update_build_number(other_action: other_action, project_information: project_information, platform_type: Helper::BmHelper::CONST_PROJECT_TYPE__IOS)
+            end
+            
+            def self.description
+                "TODO"
+            end
+            
+            def self.authors
+                ["Bemobile"]
+            end
+            
+            def self.return_value
+                "TODO. If your method provides a return value, you can describe here what it does"
+            end
+            
+            def self.details
+                "TODO"
+            end
+            
+            def self.available_options
+                [
+                    FastlaneCore::ConfigItem.new(
+                        key: :project_information,
+                        env_name: "PROJECT_INFORMATION",
+                        description: "The app information including name, version",
+                        optional: false,
+                        type: BmProjectInformation)
+                ]
+            end
+            
+            def self.is_supported?(platform)
+              true
+            end
+        end
+    end
+end
